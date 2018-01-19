bb2d35fd0e61d48db940788a0dbd465dd20206e4
sunyiyou <342065733@qq.com>
2018-01-18 19:35:29 -0500

    Update clean.py

--- util/clean.py
+++ util/clean.py
@@ -2,6 +2,6 @@ import settings
 import os
 
 def clean():
-    filelist = [f for f in os.listdir(settings.OUTPUT_FOLDER) if os.path.isfile(os.path.join(settings.OUTPUT_FOLDER, f))]
+    filelist = [f for f in os.listdir(settings.OUTPUT_FOLDER) if f.endswith('mmap')]
     for f in filelist:
         os.remove(os.path.join(settings.OUTPUT_FOLDER, f))
